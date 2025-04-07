8d6d1421a004ae13291bdbbf881a9226bf678e51
Sascha Szott <szott@gmx.de>
2025-04-07 13:43:21 +0200

    letzte Änderung im Methodenkommentar zurückgenommen

--- 2025-04-10/git-update-workflow/current-datetime.py
+++ 2025-04-10/git-update-workflow/current-datetime.py
@@ -3,7 +3,6 @@ from datetime import datetime
 def main():
     """
     Diese Funktion gibt die aktuelle Systemzeit.
-    Es wird kein Argument erwartet.
     """
     now = datetime.now()    
     print("Aktuelle Systemzeit:", now.strftime("%H:%M:%S"))    

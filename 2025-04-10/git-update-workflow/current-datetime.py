313b7a58e6c95592bf4ac66a66ca68c7e2b0f63b
Sascha Szott <szott@gmx.de>
2025-04-07 12:32:33 +0200

    Füge Docstring zur Hauptfunktion hinzu, um die Ausgabe der aktuellen Systemzeit und des Datums zu beschreiben

--- 2025-04-10/git-update-workflow/current-datetime.py
+++ 2025-04-10/git-update-workflow/current-datetime.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 
 def main():
+    """
+    Diese Funktion gibt die aktuelle Systemzeit und das aktuelle Datum aus.
+    """
     now = datetime.now()
     # ein Kommentar, der vom Dozenten eingefügt wurde <-- weitere Änderung
     print("Aktuelle Systemzeit:", now.strftime("%H:%M:%S"))

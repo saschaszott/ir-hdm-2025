230222e985454f35a925028246032b80082fe012
Sascha Szott <szott@gmx.de>
2025-04-09 23:27:09 +0200

    Änderungen zurückgesetzt auf den Anfangszustand

--- 2025-04-10/git-update-workflow/current-datetime.py
+++ 2025-04-10/git-update-workflow/current-datetime.py
@@ -1,11 +1,8 @@
 from datetime import datetime
 
 def main():
-    """
-    Diese Funktion gibt die aktuelle Systemzeit.
-    """
-    now = datetime.now()    
-    print("Aktuelle Systemzeit:", now.strftime("%H:%M:%S"))    
+    now = datetime.now()
+    print("Aktuelle Systemzeit:", now.strftime("%H:%M:%S"))
 
 if __name__ == "__main__":
     main()

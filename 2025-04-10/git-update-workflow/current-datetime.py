314d468fa86123c9bec4d50a6b77374cf0f7927c
Sascha Szott <szott@gmx.de>
2025-04-10 19:46:05 +0200

    Änderungen zurückgesetzt

--- 2025-04-10/git-update-workflow/current-datetime.py
+++ 2025-04-10/git-update-workflow/current-datetime.py
@@ -1,11 +1,8 @@
-# Dozent foobar
 from datetime import datetime
 
 def main():
     now = datetime.now()
-    # Kommentar vom Dozenten
     print("Aktuelle Systemzeit:", now.strftime("%H:%M:%S"))
-    print("Aktuelles Datum:", now.strftime("%Y-%m-%d")) # <-- neue Zeile hinzugefügt
 
 if __name__ == "__main__":
     main()

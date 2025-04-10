d979826a11ce8c33348643184567d631da690da7
Sascha Szott <szott@gmx.de>
2025-04-10 19:07:38 +0200

    Update current-datetime.py

--- 2025-04-10/git-update-workflow/current-datetime.py
+++ 2025-04-10/git-update-workflow/current-datetime.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 def main():
     now = datetime.now()
+    # Kommentar vom Dozenten
     print("Aktuelle Systemzeit:", now.strftime("%H:%M:%S"))
     print("Aktuelles Datum:", now.strftime("%Y-%m-%d")) # <-- neue Zeile hinzugefügt
 

36b170c58efa21583d3e3fa096416da9d37e2123
Sascha Szott <szott@gmx.de>
2025-04-19 19:59:40 +0200

    added several parser implementations

---
+++ 2025-04-24/03_lark.py
@@ -0,0 +1,29 @@
+from lark import Lark, UnexpectedInput
+
+# Formulierung der Grammatik für boolesche Suchanfragen in BNF
+grammar = """
+    ?start: expr
+    ?expr: expr "OR" term   -> or_expr
+         | term
+    ?term: term "AND" factor -> and_expr
+         | factor
+    ?factor: "NOT" factor    -> not_expr
+           | "(" expr ")"
+           | VAR
+
+    %import common.CNAME -> VAR
+    %import common.WS
+    %ignore WS
+"""
+
+def parse_boolean_query(query):
+    try:
+        parser = Lark(grammar, parser="lalr")
+        tree = parser.parse(query)
+        print(tree.pretty())
+    except UnexpectedInput as e:
+        print(f"Syntaxfehler in Suchanfrage:\n{e}")
+
+if __name__ == "__main__":
+    query = input("Boolesche Suchanfrage: ")
+    result = parse_boolean_query(query)

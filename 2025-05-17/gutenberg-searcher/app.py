71de9c5704921c42b0ba58fa42dea794f2881415
Sascha Szott <szott@gmx.de>
2025-05-17 20:16:15 +0200

    Füge Paginierung zur Suchergebnisseite hinzu und verbessere die Anzeige der Ergebnisse

--- 2025-05-17/gutenberg-searcher/app.py
+++ 2025-05-17/gutenberg-searcher/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 import pysolr
-
+import math
 app = Flask(__name__)
 
 solr_host = "localhost"
@@ -16,8 +16,10 @@ def index():
     return render_template("index.html")
 
 @app.route("/", methods=["POST"])
+@app.route("/search", methods=["GET"])
 def search():
     query = request.form.get("query", "")  # Suchanfrage aus dem Formular
+    page = int(request.args.get("page", 1)) # Page-Nummer aus den URL-Parametern
     results = []
     if not query:
         query = "*:*"  # Alle Dokumente zurückgeben
@@ -27,18 +29,21 @@ def search():
             "qf": "title^5 author_names^3 summaries^2 fulltext subjects", # Gewichtung der Felder, die durchsucht werden
             "defType": "edismax", # erweiterte DisMax-Suche            
             "fl": "*,score", # Felder, die zurückgegeben werden
-            "debugQuery": "true"
+            "debugQuery": "true",
+            "bf": "log(download_count)^2",
+            "start": (page - 1) * 10, # Start-Offset für die Paginierung
         }
 
         solr_results = solr.search(query, **params)
         explanations = solr_results.raw_response.get('debug', {}).get('explain', {})
+        total_pages = math.ceil(solr_results.hits / 10)
         
         for solr_result in solr_results.docs:
             result = solr_result
             result['explanation'] = explanations.get(result['id'], "Keine Erklärung verfügbar.")
             results.append(result)
 
-    return render_template("index.html", query=query, results=results, num_of_hits=solr_results.hits)
+    return render_template("index.html", query=query, results=results, num_of_hits=solr_results.hits, page=page, total_pages=total_pages, explanations=explanations)
 
 if __name__ == "__main__":
     app.run(debug=True)
